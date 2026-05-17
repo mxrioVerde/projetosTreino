7f3b14333be4a6a4e2fd36bd95a5734833a5f617
mxrioVerde <roxoluigi@gmail.com>
2026-05-17 19:54:52 -0300

    Questão adicionada

--- sequênciaQuadrados.py
+++ sequênciaQuadrados.py
@@ -1,6 +1,7 @@
+#1. Dada uma sequência de números inteiros não-nulos, seguida por 0, imprimir seus quadrados. 
 sequencia = input('Digite um número, ou 0 para encerrar o programa.')
 sq = ''
-if sequencia != 0:
+if sequencia != 0 and sequencia != '':
     while sequencia != '0':
         digito = int(sequencia)
         sq += str(digito**2) + ','
